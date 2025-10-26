5ebb541ab58c1febe26876c1e65ee82fd655e1c6
FARIA0207 <joao.miguel.ribeiro.marinho.faria@gmail.com>
2025-10-26 21:08:36 +0000

    Criação de duas Classes Enum de valroes fixos, para os estados das ofertas e das candidaturas

---
+++ src/main/java/pt/upt/equipa13/portalestagios/model/EstadoCandidatura.java
@@ -0,0 +1,9 @@
+package pt.upt.equipa13.portalestagios.model;
+
+/** Ciclo de vida de uma candidatura. */
+public enum EstadoCandidatura {
+    SUBMETIDA,
+    EM_ANALISE,
+    ACEITE,
+    REJEITADA
+}
---
+++ src/main/java/pt/upt/equipa13/portalestagios/model/EstadoOferta.java
@@ -0,0 +1,7 @@
+package pt.upt.equipa13.portalestagios.model;
+
+/** Conjunto fechado de estados possíveis de uma Oferta. */
+public enum EstadoOferta {
+    ABERTA,
+    FECHADA
+}
